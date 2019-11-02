7ca33bcc522f17017a0810db55c54cd5b3b1cc77
Luciano Witt Gonçalves <lucianowitt@gmail.com>
2019-11-02 13:26:49 -0300

    Link para a foto no dataset

--- Trabalho2.py
+++ Trabalho2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly as py
@@ -23,9 +24,23 @@ df.head()
 nulos = pd.DataFrame(df.isna().sum(), columns=['Nulos']).transpose()
 nulos
 
+# Transforma coluna Year de float para int
 df['Year'] = df['Year'].astype('int64')
-df.head()
 
+# Remove asteriscos dos nomes dos jogadores
+df['Player'] = df['Player'].str.replace('*','')
+
+# Cria coluna dos links para as fotos
+def build_photo_link(name):
+    name_parts = name.split(' ')
+    first = name_parts[0]
+    last = name_parts[1]
+    photo_link = 'https://nba-players.herokuapp.com/players/' + last + '/' + first
+    return photo_link
+
+df['Photo'] = df['Player'].apply(build_photo_link)
+
+# Cria lista dos nomes de jogadores
 players = df['Player'].sort_values().unique()
 print('Jogadores:', len(players))
 
@@ -45,6 +60,14 @@ app.layout = html.Div(children=[
                 {'x': player['Year'], 'y': player['PTS'], 'type': 'bar', 'name': player_name}
             ]
         }
+    ),
+    dash_table.DataTable(
+        id="table",
+        columns=[{"name": i, "id": i} for i in player.columns],
+        data=player.to_dict('records'),
+    ),
+    html.Img(
+        src=player['Photo'].to_list()[0]
     )
 ])
 if __name__ == '__main__':
